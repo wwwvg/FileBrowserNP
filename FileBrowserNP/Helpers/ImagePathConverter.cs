01b2ed6f9d9d187fccf0134dbada0202b9f1d7b0
Vasily <72005072+wwwvg@users.noreply.github.com>
2022-06-10 14:59:28 +0300

    Промежуточное сохранение

---
+++ FileBrowserNP/Helpers/ImagePathConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FileBrowserNP.Helpers
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        private string imageDirectory = Directory.GetCurrentDirectory();
+        public string ImageDirectory
+        {
+            get { return imageDirectory; }
+            set { imageDirectory = value; }
+        }
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string imagePath = Path.Combine(ImageDirectory,
+            (string)value);
+       
+            return new BitmapImage(new Uri(imagePath));
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+    
+}
---
+++ FileBrowserNP/Selectors/StyleTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using FileBrowserNP.Models;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FileBrowserNP.Selectors
+{
+    public class StyleTemplateSelector : StyleSelector
+    {
+        public Style DriveStyle { get; set; }
+        public Style FolderStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+
+            Window window = Application.Current.MainWindow;
+            Base product = (Base)item;
+
+            Type type = product.GetType();
+            if (type == typeof(Drive))
+                return DriveStyle;
+
+            return FolderStyle;
+        }
+    }
+}
